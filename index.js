a9040c5c90fbd72cdc3b6448f7f02e3129126ab1
Mugambi <javan.mugambi@student.moringaschool.com>
2022-11-27 15:41:18 +0300

    done test challenge

---
+++ index.js
@@ -0,0 +1,29 @@
+function calculateGrade(){
+	let grade = document.getElementById("grade").value;
+    
+	    if (grade > 100) {
+	       document.getElementById("result").innerHTML = `Invalid.`
+	    }
+	    else if (grade >= 79) {
+		document.getElementById("result").innerHTML = `Your grade is A.`
+	     }
+	    else if (grade >= 60 ) {
+		document.getElementById("result").innerHTML = `Your grade is B`
+	    }
+	    else if (grade >= 49) {
+		document.getElementById("result").innerHTML = `Your grade is C.`
+	    }
+	    else if (grade >= 40) {
+		document.getElementById("result").innerHTML = `Your grade is D.`
+	    }
+	    else if  (grade > 0 ) {
+		document.getElementById("result").innerHTML = `Your grade is E.`
+	    }
+	    else if (grade < 0) {
+		document.getElementById("result").innerHTML = `Invalid.`
+	     }
+	    else {
+		document.getElementById("result").innerHTML = `Invalid`
+	    }
+    
+    }
